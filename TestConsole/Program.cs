d067faf05684df5bdbd2c169afb01eb23ed34665
belopyckich <belopyckich@gmail.com>
2021-09-12 01:45:46 +0300

    тест парсинга базы данных

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace TestConsole
 {
@@ -6,7 +9,24 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] files = Directory.GetFiles(Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\data")));
+            int days = files.Length;
+            List<Person> monthdata = new List<Person>();
+            for (int i = 0; i < files.Length; i++)
+            {
+                var persons = JsonConvert.DeserializeObject<List<Person>>(File.ReadAllText(files[i]));
+                monthdata.AddRange(persons);
+            }
+            foreach (Person day in monthdata)
+            {
+                Console.WriteLine(day.GetData());
+            }
+            for(int i = 0; i < monthdata.Count; i++)
+            {
+                if()
+            }
+            Console.ReadKey();
+
         }
     }
 }
